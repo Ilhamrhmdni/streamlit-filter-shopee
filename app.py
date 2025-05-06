7e15523b511128e1f88230e853533924b631c001
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-05-06 18:37:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import io
 
-st.set_page_config(page_title="Iso-iso Westo", layout="wide")
+st.set_page_config(page_title="Filter Produk", layout="wide")
 
 # === CSS untuk tema gelap tetap ===
 st.markdown("""
